f39cfab9c2a8d0f9afb1066ece46e6981bceaed1
klknn <klknn@users.noreply.github.com>
2024-08-12 12:57:46 +0000

    deploy: 3ed5c2ce8cee4e8e8d6df40c73d82b1d49aebe3c

--- js/hz2cent.js
+++ js/hz2cent.js
@@ -1,16 +1,13 @@
-function hz2cent() {
+var cent = document.getElementById('cent');
+var target = document.getElementById('target');
+var source = document.getElementById('source');
+
+function hz2cent(unused) {
     var src = parseFloat(source.value);
     var tgt = parseFloat(target.value);
     cent.innerText = 12 * Math.log2(tgt / src);
 }
 
-var cent = document.getElementById('cent');
-var target = document.getElementById('target');
-var source = document.getElementById('source');
 hz2cent();
-source.addEventListener('keyup', function(e) {
-    hz2cent();
-});
-target.addEventListener('keyup', function(e) {
-    hz2cent();
-});
+source.addEventListener('keyup', hz2cent);
+target.addEventListener('keyup', hz2cent);
